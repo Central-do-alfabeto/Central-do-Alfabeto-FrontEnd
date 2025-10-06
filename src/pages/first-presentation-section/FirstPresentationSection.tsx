42735d2905b28aadd4bc477639b049fbdd4043fc
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-10-06 20:23:40 -0300

    Update FirstPresentationSection.tsx

--- src/pages/first-presentation-section/FirstPresentationSection.tsx
+++ src/pages/first-presentation-section/FirstPresentationSection.tsx
@@ -3,8 +3,6 @@ import { playAudio } from "../../utils/playAudio";
 import { useAudioRunning } from "../../state/useAudioRunning";
 import { useShowText } from "../../state/useShowText";
 
-
-
 export function FirstPresentationSection() {
 const [audioRunning, setAudioRunning] = useAudioRunning();
 const [showText] = useShowText();
